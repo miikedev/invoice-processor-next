5bcad8caefc4029e656f01865a5f41793cd0f25c
mikedevv <maungdevv@gmail.com>
2025-08-22 14:03:46 +0630

    fix: compile fails

--- src/app/dashboard/overview/page.tsx
+++ src/app/dashboard/overview/page.tsx
@@ -1,14 +1,7 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { insertInvoiceData } from '@/db/connect';
-import Image from 'next/image';
-import Link from 'next/link';
-import { useState, useRef } from 'react';
 import { useRouter } from 'next/navigation'
-import DocSearchingAnimation from '@/components/doc-searching-animation';
-import Video from '@/components/video';
 export default function OverviewPage() {
 
 const router = useRouter()
